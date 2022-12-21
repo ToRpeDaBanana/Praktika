2b64969182e8da84f47b33136a91b5d8ec946b18
Хмелёв Алексей <khmelyow2017@yandex.ru>
2022-12-21 22:48:44 +0700

    Удаление ссылки на проект

---
+++ Form7.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace UDP_Chat
+{
+    partial class Form7
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.logoutButton = new System.Windows.Forms.Button();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.chatTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите имя: ";
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Location = new System.Drawing.Point(117, 30);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(278, 20);
+            this.userNameTextBox.TabIndex = 1;
+            this.userNameTextBox.TextChanged += new System.EventHandler(this.userNameTextBox_TextChanged);
+            this.userNameTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.userNameTextBox_KeyDown);
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(435, 28);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 2;
+            this.loginButton.Text = "Вход";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // logoutButton
+            // 
+            this.logoutButton.Location = new System.Drawing.Point(435, 66);
+            this.logoutButton.Name = "logoutButton";
+            this.logoutButton.Size = new System.Drawing.Size(75, 23);
+            this.logoutButton.TabIndex = 3;
+            this.logoutButton.Text = "Выход";
+            this.logoutButton.UseVisualStyleBackColor = true;
+            this.logoutButton.Click += new System.EventHandler(this.logoutButton_Click);
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(414, 371);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(110, 39);
+            this.sendButton.TabIndex = 4;
+            this.sendButton.Text = "Отправить";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Location = new System.Drawing.Point(12, 381);
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(396, 20);
+            this.messageTextBox.TabIndex = 5;
+            this.messageTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.messageTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.messageTextBox_KeyDown);
+            // 
+            // chatTextBox
+            // 
+            this.chatTextBox.Location = new System.Drawing.Point(12, 116);
+            this.chatTextBox.Multiline = true;
+            this.chatTextBox.Name = "chatTextBox";
+            this.chatTextBox.Size = new System.Drawing.Size(512, 230);
+            this.chatTextBox.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 365);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Введите сообщение";
+            // 
+            // Form7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 420);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chatTextBox);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.logoutButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.userNameTextBox);
+            this.Controls.Add(this.label1);
+            this.KeyPreview = true;
+            this.Name = "Form7";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button logoutButton;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.TextBox chatTextBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form7.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UDP_Chat
+{
+    public partial class Form7 : Form
+    {
+        bool alive = false;
+        UdpClient client;
+        const int LOCALPORT = 8001;
+        const int REMOTEPORT = 8001;
+        const int TTL = 20;
+        const string HOST = "235.5.5.1";
+        IPAddress groupAddress;
+
+        string userName;
+
+        public Form7()
+        {
+            InitializeComponent();
+
+            loginButton.Enabled = true;
+            logoutButton.Enabled = false;
+            sendButton.Enabled = false;
+            chatTextBox.ReadOnly = true;
+
+            groupAddress = IPAddress.Parse(HOST);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            chatTextBox.ScrollBars = ScrollBars.Vertical;
+            chatTextBox.AcceptsReturn = true;
+            chatTextBox.WordWrap = true;
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            userName = userNameTextBox.Text;
+            userNameTextBox.ReadOnly = true;
+
+            try
+            {
+                client = new UdpClient(LOCALPORT);
+                client.JoinMulticastGroup(groupAddress, TTL);
+
+                Task receiveTask = new Task(ReceiveMessages);
+                receiveTask.Start();
+
+                string message = userName + " присоединяется к нам!";
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                client.Send(data, data.Length, HOST, REMOTEPORT);
+
+                loginButton.Enabled = false;
+                logoutButton.Enabled = true;
+                sendButton.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ReceiveMessages()
+        {
+            alive = true;
+            try
+            {
+                while (alive)
+                {
+                    IPEndPoint remoteIp = null;
+                    byte[] data = client.Receive(ref remoteIp);
+                    string message = Encoding.Unicode.GetString(data);
+
+                    this.Invoke((MethodInvoker) delegate
+                        {
+                        string time = DateTime.Now.ToShortTimeString();
+                        chatTextBox.Text = time + " " + message + "\r\n" + chatTextBox.Text;
+                        });
+                }
+            }
+            catch (ObjectDisposedException)
+            {
+                if (!alive)
+                    return;
+                throw;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string message = String.Format("{0}: {1}", userName, messageTextBox.Text);
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                client.Send(data, data.Length, HOST, REMOTEPORT);
+                messageTextBox.Clear();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void logoutButton_Click(object sender, EventArgs e)
+        {
+            ExitChat();
+        }
+
+        private void ExitChat()
+        {
+            string message = userName + " покидает нас!";
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            client.Send(data, data.Length, HOST, REMOTEPORT);
+            client.DropMulticastGroup(groupAddress);
+
+            alive = false;
+            client.Close();
+
+            loginButton.Enabled = true;
+            logoutButton.Enabled = false;
+            sendButton.Enabled = false;
+            userNameTextBox.ReadOnly = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosedEventArgs e)
+        {
+            if (alive)
+                ExitChat();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Внутренний чат";
+        }
+
+        private void userNameTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == (char)Keys.Enter)
+            {
+                loginButton_Click(loginButton, null);
+                messageTextBox.Focus();
+            }
+        }
+
+        private void messageTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == (char)Keys.Enter)
+            {
+                sendButton_Click(sendButton, null);
+            }
+        }
+
+        private void userNameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
